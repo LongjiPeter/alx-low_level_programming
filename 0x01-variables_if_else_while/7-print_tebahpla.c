5b7ec0e319937561f23963ac57e2aa82a66322b9
Longji <stuntmandajab@gmail.com>
2022-09-12 17:48:22 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char start;
+
+for (start = 'z'; z >= 'a'; z--)
+putchar(start);
+putchar('\n');
+
+return (0);
+}
