91696c10682332f4068efd512e84f01de13529f2
Longji <stuntmandajab@gmail.com>
2022-09-12 18:01:32 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * return: always 0 (success)
+ */
+
+int main(void)
+{
+int numbers;
+char letters;
+
+for (numbers = '0'; numbers <= '9'; numbers++)
+putchar(numbers);
+for (letters = 'a'; letters <= 'f'; leters++)
+putchar(letters);
+putchar('\n');
+
+return (0);
