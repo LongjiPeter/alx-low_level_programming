52c10a58cdd560d9b00b2e2ab48272e3c5efa52d
Longji <stuntmandajab@gmail.com>
2022-09-15 16:52:46 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests thefunction that prints if a number is positive or negative
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+i = 3;
+positive_or_negative(1);
+
+return (0);
+}
