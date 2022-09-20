b496f4dbc6cf7177657ae260aa280a30c67e814e
Longji <stuntmandajab@gmail.com>
2022-09-19 21:41:34 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array
+ * @a: integer value
+ * @n: integer value
+ */
+
+void print_array(int *a, int n)
+{
+int b;
+for (b = 0; b < n; b++)
+{
+printf("%d", a[b]);
+if (b != (n - 1))
+printf(", ");
+}
+printf("\n");
+}
